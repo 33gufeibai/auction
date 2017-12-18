435c3371ad975c34a21a4e3ffded9f62438f0b45
gufeibai <1294764056@qq.com>
2017-12-18 23:56:30 +0800

    updated date ——更新评星组件

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -6,13 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stars.component.css']
 })
 export class StarsComponent implements OnInit {
+
+  // 星级评价的rating属性的值应该由它的父组件传递给他
+  // @Input() content:number;
+  private rating:number = 0
+
   private stars: boolean[]
 
   constructor() { }
 
   ngOnInit() {
-
-    this.stars = [true,true,true,true,true]
+    // this.stars = []
+    // for(let i = 1; i <= 5; i++){
+    //   this.stars.push(i > this.rating)
+    // }
+    // this.stars = [true,true,true,true,true]
   }
 
 }
